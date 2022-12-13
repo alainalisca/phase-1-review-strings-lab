6217c118d79b567f36758e6d53c5f4321ee0578d
Alain Alisca <alainalisca@gmail.com>
2022-12-13 05:15:55 -0500

    commit tree 4bf6f61a4f7558e1493eadf0af11ed967cd130e2

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Write your code in this file!
+const currentUser = "Grace Hopper ";
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const myString = 'template literal';
+
+const myNumber = 10; 
+const myBoolean = false; 
+
+`Saying that interpolation with ${myString}s is better than concatenation ${90 + myNumber}% of the time is simply ${myBoolean}. But it is pretty cool!`
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`;
